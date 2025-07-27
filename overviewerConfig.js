11e1febbfd3c48e292020675bc7a10d6cc4a641c
guinea-ckaledonia <zoyre28430@gmail.com>
2025-07-27 21:46:07 +0200

    Add files via upload

---
+++ overviewerConfig.js
@@ -0,0 +1,73 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "World"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1753626056",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "attribution": null
+    },
+    "tilesets": [
+        {
+            "name": "North",
+            "zoomLevels": 20,
+            "defaultZoom": 14,
+            "maxZoom": 20,
+            "path": "north",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "World",
+            "last_rendertime": 1753625152,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -13,
+                112,
+                213
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 14,
+            "spawn": [
+                -13,
+                112,
+                213
+            ],
+            "north_direction": 0
+        }
+    ]
+};
